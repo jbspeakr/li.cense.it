a78270daee168ddb702ea2116f4b58bf09823372
jbspeakr <jan@brennenstuhl.me>
2015-04-23 11:41:11 +0200

    first incomplete implementation of license fetching for Github users.

--- js/main.js
+++ js/main.js
@@ -1,25 +1,87 @@
 'use strict';
 
-angular.module('licker', ['ngRoute'])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/table.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise('/');
-  }).controller('MainCtrl', function ($scope, $http) {
-        $scope.$watch('search', function (input) {
-            var domainhacks = [];
-
-            angular.forEach($scope.domains, function (domain){
-                var match = input.toLowerCase().match(domain['tld']);
-                if(match){
-                    var domainhack = domainhackBuilder.make(input, domain);
-                    domainhacks.push(domainhack);
-                }
-            });
-
-            $scope.domainhacks = domainhacks;
+var app = angular.module('licker', ['ngRoute'])
+    .config(function ($routeProvider) {
+      $routeProvider
+          .when('/', {
+            templateUrl: 'views/table.html',
+            controller: 'MainCtrl'
+          })
+          .otherwise('/');
     });
-});
+
+app.controller('mainCtrl', ['$scope', 'githubApiConnector', function ($scope, githubApiConnector) {
+  $scope.githubHandle = '';
+  $scope.repositories = [];
+  $scope.fetched_repositories = [];
+
+  $scope.checkLicenses = function () {
+
+    var page_parameter = '&page=';
+
+    githubApiConnector.fetchReposForUser($scope.githubHandle).
+        success(function (data) {
+          $scope.fetched_repositories = data;
+        }).
+        error(function () {
+          githubApiConnector.fetchReposForOrganisation($scope.githubHandle).
+              success(function (data) {
+                $scope.fetched_repositories = data;
+              }).
+              error(function () {
+                $scope.fetched_repositories = $scope.fetched_repositories.push({
+                  'name': 'test-repo',
+                  'link': 'http://www.google.de',
+                  'license': 'Apache License v2'
+                });
+              })
+        });
+
+  };
+  $scope.$watch('fetched_repositories', function () {
+    angular.forEach($scope.fetched_repositories, function (repository) {
+      githubApiConnector.getLicenseForRepo(repository).
+          success(function (data) {
+            $scope.repositories.push(data);
+          }).
+          error(function () {
+            alert('error.')
+          });
+    });
+  });
+}]);
+
+
+app.factory('githubApiConnector', ['$http', function ($http) {
+  var api = 'https://api.github.com';
+
+  return {
+    fetchReposForUser: function (githubHandle) {
+      var user_repo_endpoint = api + '/users/' + githubHandle + '/repos';
+      var request = {
+        method: 'GET',
+        url: user_repo_endpoint
+      };
+      return $http(request);
+    },
+    fetchReposForOrganisation: function (githubHandle) {
+      var organisation_repo_endpoint = api + '/orgs/' + githubHandle + '/repos';
+      var request = {
+        method: 'GET',
+        url: organisation_repo_endpoint
+      };
+      return $http(request);
+    },
+    getLicenseForRepo: function (repo) {
+      var license_endpoint = api + '/repos/' + repo['full_name']
+      var request = {
+        method: 'GET',
+        url: license_endpoint,
+        headers: {
+          'Accept': 'application/vnd.github.drax-preview+json'
+        }
+      };
+      return $http(request);
+    }
+  };
+}]);
