6b0bd04a8aec19722898198797408410b3de3978
jbspeakr <jan@brennenstuhl.me>
2015-04-23 19:34:05 +0200

    fix paging.

--- js/main.js
+++ js/main.js
@@ -10,52 +10,61 @@ var app = angular.module('licker', ['ngRoute'])
           .otherwise('/');
     });
 
-app.controller('mainCtrl', ['$scope', 'githubApiConnector', function ($scope, githubApiConnector) {
+app.controller('mainCtrl', ['$scope', '$q', 'githubApiConnector', function ($scope, $q, githubApiConnector) {
   $scope.githubHandle = '';
+  $scope.errorMessage = '';
   $scope.repositories = [];
   $scope.fetched_repositories = [];
 
+  $scope.fetchRepos = function (apiPage, repositories) {
+    var defaultResultLength = 30;
+    var page = apiPage || 1;
+    var repositories = repositories || [];
+
+    githubApiConnector.fetchReposForUser($scope.githubHandle, page).
+        success(function (data) {
+          if (data.length > 0) {
+            repositories = repositories.concat(data);
+          }
+          if (data.length == defaultResultLength) {
+            $scope.fetchRepos(++page, repositories);
+          } else{
+            $scope.updateFetchedRepositories(repositories);
+          }
+        }).
+        error(function (data, status) {
+          if (status == 403) {
+            $scope.errorMessage = 'Not able to get complete information: Github just allows you to make up to 60 requests per hour.';
+          }
+        });
+  };
+
   $scope.checkLicenses = function () {
     $scope.fetched_repositories = [];
+    $scope.repositories = [];
+    $scope.fetchRepos();
+  };
 
-    var page = 1;
+  $scope.updateFetchedRepositories = function (repositories) {
+    $scope.fetched_repositories = repositories;
+  };
 
-    do {
-      githubApiConnector.fetchReposForUser($scope.githubHandle, page).
-          success(function (data) {
-            $scope.fetched_repositories = data;
-          }).
-          error(function () {
-            $scope.fetched_repositories = $scope.fetched_repositories;
-          });
-      page++;
-    } while ($scope.fetched_repositories.length !== $scope.fetched_repositories.length);
-
-    page = 1;
-    if ($scope.fetched_repositories.length < 1) {
-      do {
-        githubApiConnector.fetchReposForOrganisation($scope.githubHandle, page).
-            success(function (data) {
-              $scope.fetched_repositories = data;
-            }).
-            error(function () {
-              $scope.fetched_repositories = $scope.fetched_repositories;
-            });
-        page++;
-      } while ($scope.fetched_repositories.length !== $scope.fetched_repositories.length);
-    }
+  $scope.updateRepositories = function (repositories) {
+    $scope.repositories = repositories;
   };
 
   $scope.$watch('fetched_repositories', function () {
+    var repositories = [];
     angular.forEach($scope.fetched_repositories, function (repository) {
       githubApiConnector.getLicenseForRepo(repository).
           success(function (data) {
-            $scope.repositories.push(data);
+            repositories.push(data);
           }).
           error(function () {
             alert('error.')
           });
     });
+    $scope.updateRepositories(repositories);
   });
 }]);
 
@@ -69,7 +78,10 @@ app.factory('githubApiConnector', ['$http', function ($http) {
       var user_repo_endpoint = api + '/users/' + githubHandle + '/repos' + page_parameter + page;
       var request = {
         method: 'GET',
-        url: user_repo_endpoint
+        url: user_repo_endpoint,
+        headers: {
+          'Authorization': 'token 5c9bd3c829fa739b7d63aedd03c6ad1c50b7c292'
+        }
       };
       return $http(request);
     },
@@ -77,7 +89,10 @@ app.factory('githubApiConnector', ['$http', function ($http) {
       var organisation_repo_endpoint = api + '/orgs/' + githubHandle + '/repos' + page_parameter + page;
       var request = {
         method: 'GET',
-        url: organisation_repo_endpoint
+        url: organisation_repo_endpoint,
+        headers: {
+          'Authorization': 'token 5c9bd3c829fa739b7d63aedd03c6ad1c50b7c292'
+        }
       };
       return $http(request);
     },
@@ -87,7 +102,8 @@ app.factory('githubApiConnector', ['$http', function ($http) {
         method: 'GET',
         url: license_endpoint,
         headers: {
-          'Accept': 'application/vnd.github.drax-preview+json'
+          'Accept': 'application/vnd.github.drax-preview+json',
+          'Authorization': 'token 5c9bd3c829fa739b7d63aedd03c6ad1c50b7c292'
         }
       };
       return $http(request);
